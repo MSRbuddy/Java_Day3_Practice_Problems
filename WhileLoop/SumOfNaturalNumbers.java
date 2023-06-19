460c684b47f769b965d339ebc18304cd9e4c2a50
MeghanaDasari <meghanadasari222@gmail.com>
2023-06-19 11:56:46 +0530

    Added : Java Practice Problems using WHILE-LOOP!

---
+++ WhileLoop/SumOfNaturalNumbers.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class SumOfNaturalNumbers {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the value of N: ");
+        int N = scanner.nextInt();
+
+        int sum = 0;
+        int i = 1;
+
+        while (i <= N) {
+            sum += i;
+            i++;
+        }
+
+        System.out.println("The sum of the first " + N + " natural numbers is: " + sum);
+    }
+}
